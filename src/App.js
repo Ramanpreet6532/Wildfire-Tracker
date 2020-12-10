389c40a3f7b7392f2f91c03269d4bf98a6ba495e
Ramanpreet6532 <rammugill72@gmail.com>
2020-12-10 19:58:42 +0530

    Fetch Data from Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import Map from './components/Map';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+    console.log({ eventData })
+
+  }, [])
+
   return (
     <div className="App">
-      <h1>Wildfire Tracker</h1>
+      <Map />
     </div>
   );
 }
